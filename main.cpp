8d87c1479bdc42e59afb565c28d3541ddd667d83
Ronak Vataliya <ronak.vataliya543@gmail.com>
2026-01-07 05:08:52 +0530

    Complete Layered Structure implemented

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include"utils/utils.h"
+#include"disk/disk.h"
+#include"fs/fs.h"
+#include"cli/cli.h"
+
+using namespace std;
+
+int main() {
+    cout << "Layered Structure finished" << endl;
+    return 0;
+}
